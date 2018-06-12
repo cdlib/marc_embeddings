04b33eac420b1a524cf7e4cb575d711b1ef6da6e
Michael Morris-Pearce <michael.morris-pearce@ucop.edu>
2018-06-12 13:58:08 -0700

    Updates to Zephir utilities.

--- marc_embeddings/zephyr.py
+++ marc_embeddings/zephyr.py
@@ -3,6 +3,8 @@ from functools import reduce
 from urllib3 import PoolManager
 import pandas as pd
 from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def pick_field(array, name):
@@ -54,13 +56,69 @@ class FlattenTransformer(BaseEstimator, TransformerMixin):
         return list(reduce(lambda a, b: a + b, records))
 
 
-API_HOST = ('http', 'zephir', 'cdlib', 'org')
+API_HOST = ('http', 'd2d-zephir-stg', 'cdlib', 'org')
 API_URI = "/api/item/%s.json"
 API_URL = ("%s://%s.%s.%s" % API_HOST) + API_URI
 
 http = PoolManager()
 
+import sys
 
 def load_from_api(htid):
-    response = http.request('GET', API_URL % htid)
+    urlstr = API_URL % htid
+    sys.stderr.write("GET %s\n" % urlstr)
+    sys.stderr.flush()
+    response = http.request('GET', urlstr)
     return ZephyrRecord(metadata=json.loads(response.data.decode('utf-8')))
+
+
+# Compares two vectors.
+class CosineSimilarity(BaseEstimator, TransformerMixin):
+    @classmethod
+    def evaluate(cls, x):
+        v_size = int(x.shape[1] / 2)
+        v1 = x[:, :v_size]
+        v2 = x[:, v_size:]
+        return cosine_similarity(v1, v2)[0]
+
+    def transform(self, X):
+        return list(map(lambda x: self.evaluate(x), X))
+
+    def fit(self, *_):
+        return self
+
+
+class SelectField(BaseEstimator, TransformerMixin):
+    def __init__(self, field):
+        self.index = field
+
+    def transform(self, metadata):
+        return list(map(lambda r: r[self.index], metadata))
+
+    def fit(self, *_):
+        return self
+
+
+LEFT = 0
+RIGHT = 1
+
+
+# Operates on a pair of records (as a tuple).
+def compare_field(field, vectorizer):
+    return Pipeline([
+            ('split r1, r2', FeatureUnion([
+                ('r1', Pipeline([
+                    ('select r1', SelectField(LEFT)),
+                    ('get field', ZephyrTransformer([field])),
+                    ('flatten', FlattenTransformer()),
+                    ('vectorize', vectorizer)
+                ])),
+                ('r2', Pipeline([
+                    ('select r2', SelectField(RIGHT)),
+                    ('get field', ZephyrTransformer([field])),
+                    ('flatten', FlattenTransformer()),
+                    ('vectorize', vectorizer)
+                ]))
+            ])),
+            ('cosine similarity', CosineSimilarity())
+        ])
